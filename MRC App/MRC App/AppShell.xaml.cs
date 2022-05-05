4b48e209496862089cebb436966c3f3994b8230b
lorinvzyl <69055042+lorinvzyl@users.noreply.github.com>
2022-05-05 14:56:42 +0200

    Deleted unnecessary pages and added relevant pages. Added these pages to the navbar for ease of editting. View Models still need to be added for these pages.

--- MRC App/MRC App/AppShell.xaml.cs
+++ MRC App/MRC App/AppShell.xaml.cs
@@ -11,8 +11,7 @@ namespace Midrand_Reformed_Church_App
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
