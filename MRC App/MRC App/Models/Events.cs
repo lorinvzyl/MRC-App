d78679bb2ffebe69d60eba3c4b46154e44f22409
lorinvzyl <69055042+lorinvzyl@users.noreply.github.com>
2022-05-21 19:50:55 +0200

    Added simple coding for eventspage, currently does not show calendar or events, but does show an arrow as well as the month/year. Fixed ZXing.Net.Mobile issue.

---
+++ MRC App/MRC App/Models/Events.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MRC_App.Models
+{
+    public class Events
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Participants { get; set; }
+    }
+}
--- MRC App/MRC App/ViewModels/EventsViewModel.cs
+++ MRC App/MRC App/ViewModels/EventsViewModel.cs
@@ -1,14 +1,60 @@
-﻿using System;
+﻿using MRC_App.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Plugin.Calendar.Models;
 
 namespace MRC_App.ViewModels
 {
-    internal class EventsViewModel : BaseViewModel
+    public class EventsViewModel : BaseViewModel
     {
         public EventsViewModel()
         {
-            Title = "Events";
+            
+        }
+
+        public ICommand TodayCommand => new Command(() =>
+        {
+            Year = DateTime.Today.Year;
+            Month = DateTime.Today.Month;
+        });
+
+        public ICommand EventSelectedCommand => new Command(async (item) => await ExecuteEventSelectedCommand(item));
+
+        private async Task ExecuteEventSelectedCommand(object item)
+        {
+            if (item is Events events)
+            {
+                //Move user to event?
+                //Or create popup where user can RSVP
+            }
+        }
+
+        public EventCollection Events { get; set; }
+
+        private int _month = DateTime.Today.Month;
+
+        public int Month
+        {
+            get { return _month; }
+            set { _month = value; }
+        }
+
+        private int _year = DateTime.Today.Year;
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+        private DateTime? _selectedDate = DateTime.Today;
+        public DateTime? SelectedDate
+        {
+            get { return _selectedDate; }
+            set { _selectedDate = value; }
         }
     }
 }
