40eeaa41da6716eb034d5a92028166dae17533ff
lorinvzyl <69055042+lorinvzyl@users.noreply.github.com>
2022-05-21 14:15:41 +0200

    Added collectionview for AboutPage, which will be used to display recent blogs to the user. Inserted fake data for now, images cause the application to be slow on the about page. This can be fixed later reducing the size of the images.

---
+++ MRC App/MRC App/Models/Blogs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MRC_App.Models
+{
+    public class Blog
+    {
+        public int ID { get; set; }
+        public string Img { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+    }
+}
--- MRC App/MRC App/ViewModels/AboutViewModel.cs
+++ MRC App/MRC App/ViewModels/AboutViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MRC_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -7,9 +10,42 @@ namespace MRC_App.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        private ObservableCollection<Blog> Blog;
+        public ObservableCollection<Blog> blog
+        {
+            get { return Blog; }
+            set { Blog = value; }
+        }
+
         public AboutViewModel()
         {
-            Title = "About";
+            blog = new ObservableCollection<Blog>();
+            AddData();
+        }
+
+        private void AddData()
+        {
+            blog.Add(new Blog
+            {
+                ID = 0,
+                Title = "How the Early Church Dealt with Racial and Cultural Division",
+                Content = "It has never been more complicated to be a pastor than it is right now. At least that’s how it often seems. As racial tensions and culture clashes have dominated the headlines in our nation, too often those unwanted guests have decided to attend our churches as well. How do we navigate our ministries to the safe harbors of peace and unity while still fulfilling our prophetic call to proclaim the truth of the gospel that challenges our tendency to elevate our norms over others? And how can Scripture equip us to address today’s racial and ethnic tensions?",
+                Img = "https://images.pexels.com/photos/6115945/pexels-photo-6115945.jpeg"
+            });
+            blog.Add(new Blog
+            {
+                ID = 1,
+                Title = "How Seminary Downsizing Cuts into Community",
+                Content = "There is no good news coming from freestanding seminaries, and there hasn’t been for some time. Gordon-Conwell Theological Seminary closure of its campus in Hamilton, Massachusetts, is simply the latest in a string of downsizing among evangelical seminaries.",
+                Img = "https://images.pexels.com/photos/208216/pexels-photo-208216.jpeg"
+            });
+            blog.Add(new Blog
+            {
+                ID = 2,
+                Title = "Foster Children of Color Need Empathy, Sensitivity and Justice",
+                Content = "I am a mother of seven, including four who were adopted after spending several years in a broken foster care system. I — like so many others who want to help, protect and minister to vulnerable children — began my journey with good intentions.",
+                Img = "https://images.pexels.com/photos/7169640/pexels-photo-7169640.jpeg"
+            });
         }
     }
 }
