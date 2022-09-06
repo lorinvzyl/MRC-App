c2d8cb9a55d16112eed9e120603e0209acd4d511
lorinvzyl <69055042+lorinvzyl@users.noreply.github.com>
2022-09-06 16:29:36 +0200

    Added base classes for custom switch.

---
+++ MRC App/MRC App/Controls/CSwitch.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Switch;
+using Switch.Helpers;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MRC_App.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CSwitch : CustomSwitch
+    {
+        public CSwitch()
+        {
+            InitializeComponent();
+        }
+    }
+}
