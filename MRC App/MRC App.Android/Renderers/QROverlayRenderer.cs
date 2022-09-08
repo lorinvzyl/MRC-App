41627e87d2336de1165a1d87e96cf1168bbff7da
lorinvzyl <69055042+lorinvzyl@users.noreply.github.com>
2022-09-08 15:47:20 +0200

    Updated naming of assembly references (it was still the old name). Added BaseQROverlay and nativeqroverlay for android.

---
+++ MRC App/MRC App.Android/Renderers/QROverlayRenderer.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Midrand_Reformed_Church_App.Droid.Renderers
+{
+    internal class QROverlayRenderer
+    {
+    }
+}
