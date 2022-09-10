5af9ade5823046c422691eea4b83b0ad8409b22a
lorinvzyl <69055042+lorinvzyl@users.noreply.github.com>
2022-09-10 15:16:58 +0200

    Fixed error with location.cs in models

--- MRC App/MRC App/Models/Location.cs
+++ MRC App/MRC App/Models/Location.cs
@@ -8,7 +8,7 @@ namespace MRC_App.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Location { get; set; }
+        public string Address { get; set; }
         public string PastorName { get; set; }
     }
 }
