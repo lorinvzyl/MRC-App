0f0741a3d49a7676e13b44b9e901ea746e4e0295
lorinvzyl <69055042+lorinvzyl@users.noreply.github.com>
2022-09-11 22:52:04 +0200

    Revert "MyPopup removed to get rid of errors. Adjusted LoginPage."
    This reverts commit 5c0913f8b8133871c479d77a3edfb6c3fd9d997d.

---
+++ MRC App/MRC App/Views/MyPopup.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MRC_App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyPopup : Popup
+    {
+        public MyPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
