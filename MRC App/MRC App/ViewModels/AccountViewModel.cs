ec4892a9d08f5065ef5ada76a1262d904ed72505
lorinvzyl <69055042+lorinvzyl@users.noreply.github.com>
2022-09-12 16:53:32 +0200

    Added delete method for user.

--- MRC App/MRC App/ViewModels/AccountViewModel.cs
+++ MRC App/MRC App/ViewModels/AccountViewModel.cs
@@ -5,7 +5,8 @@ using System.Windows.Input;
 using Xamarin.Forms;
 using Acr.UserDialogs;
 
-using System.Threading.Tasks; 
+using System.Threading.Tasks;
+using MRC_App.Services;
 
 namespace MRC_App.ViewModels
 {
@@ -15,7 +16,7 @@ namespace MRC_App.ViewModels
         {
             ToggleCommand = new Command<bool>(async x => await Toggled(x).ConfigureAwait(false));
         }
-            public bool EnableCommands { get; set; }
+        public bool EnableCommands { get; set; }
         public bool EnableEvents { get; set; }
 
         public ICommand ToggleCommand { get; }
@@ -27,6 +28,15 @@ namespace MRC_App.ViewModels
                 await UserDialogs.Instance.AlertAsync($"New value: {newValue}", "Switch toggled (Command)").ConfigureAwait(false);
             }
         }
+
+        public async Task<bool> DeleteUser(string email)
+        {
+            var delete = false;
+            if (email != null)
+                delete = await RestService.DeleteUser(email);
+
+            return delete;
+        }
     }
-    }
+}
 
