9ad494df9d42339aaa6a51cf461189c7f688429f
lorinvzyl <69055042+lorinvzyl@users.noreply.github.com>
2022-09-14 12:12:01 +0200

    added capabilities for youtube url and fixed mediaelement autoplay (it is a bug in xamarin community toolkit)

--- MRC App/MRC App/Views/AboutPage.xaml.cs
+++ MRC App/MRC App/Views/AboutPage.xaml.cs
@@ -5,6 +5,10 @@ using Xamarin.Forms.Xaml;
 using MRC_App.ViewModels;
 using MRC_App.Models;
 using System.Linq;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
 
 namespace MRC_App.Views
 {
@@ -13,11 +17,16 @@ namespace MRC_App.Views
         public AboutPage()
         {
             InitializeComponent();
+
+            var videoId = "mpDfgAjWz-Q";
+            
+            GetYoutubeUrl(videoId);
+            
         }
 
         async void ItemSelected_CollectionView(object sender, SelectionChangedEventArgs e)
         {
-            if(!(e.CurrentSelection.FirstOrDefault() is Blog blog))
+            if (!(e.CurrentSelection.FirstOrDefault() is Blog blog))
                 return;
 
             var blogdetailed = new BlogDetailed
@@ -27,6 +36,29 @@ namespace MRC_App.Views
 
             await Navigation.PushAsync(blogdetailed);
             ((CollectionView)sender).SelectedItem = null;
+        }
+
+        public async Task GetYoutubeUrl(string video)
+        {
+            var youtube = new YoutubeClient();
+
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(
+                    video
+                );
+            var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+
+            if (streamInfo != null)
+            {
+                var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+                var source = streamInfo.Url;
+
+                mediaSource.Source = source;
+            }
+        }
+
+        private void mediaSource_MediaOpened(object sender, EventArgs e)
+        {
+            ((MediaElement)sender).Speed = 0; //Xamarin community toolkit currently has a bug where autoplay being set to off doesn't work. This is a workaround.
         }
     }
 }
