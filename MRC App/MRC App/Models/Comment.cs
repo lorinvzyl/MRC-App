ceaebe33d731ee1e3440d522216a465b1b471f21
lorinvzyl <69055042+lorinvzyl@users.noreply.github.com>
2022-09-15 15:39:49 +0200

    Add blogdetailedviewmodel, updated blogdetailed.

--- MRC App/MRC App/Models/Comment.cs
+++ MRC App/MRC App/Models/Comment.cs
@@ -9,7 +9,7 @@ namespace MRC_App.Models
         public int Id { get; set; }
         public int BlogId { get; set; }
         public string CommentText { get; set; }
-        public int UserId { get; set; }
+        public string User { get; set; }
 
     }
 }
---
+++ MRC App/MRC App/ViewModels/BlogDetailedViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MRC_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace MRC_App.ViewModels
+{
+    public class BlogDetailedViewModel : BaseViewModel
+    {
+        private ObservableRangeCollection<Comment> comments;
+        public ObservableRangeCollection<Comment> Comments
+        {
+            get { return comments; }
+            set 
+            { 
+                comments = value;
+                OnPropertyChanged();
+            }
+        }
+        public BlogDetailedViewModel()
+        {
+            Comments = new ObservableRangeCollection<Comment>();
+        }
+
+        private async Task AddData()
+        {
+
+        }
+    }
+}
