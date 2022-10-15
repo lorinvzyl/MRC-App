a315a906249139dd2ba9b7a7157778277f8cd810
lorinvzyl <69055042+lorinvzyl@users.noreply.github.com>
2022-10-15 02:40:44 +0200

    Added methods that need adding for UITest

--- MRC App.UITest/Tests.cs
+++ MRC App.UITest/Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using MRC_App.Views;
 using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
@@ -11,6 +12,8 @@ namespace MRC_App.UITest
     //[TestFixture(Platform.iOS)]
     public class Tests
     {
+        //In this scenario, to avoid having to start up the app for every possible task, screenshots will be taken to verify outputs
+
         IApp app;
         Platform platform;
 
@@ -44,6 +47,114 @@ namespace MRC_App.UITest
             app.Screenshot("Register screen.");
 
             Assert.IsTrue(results.Any());
+        }
+
+        [Test]
+        public void RegisterUser()
+        {
+            //Register valid user account
+        }
+
+        [Test]
+        public void LoginAndExplore()
+        {
+            //Login user and navigate to all possible pages
+        }
+
+        [Test]
+        public void TestValidation()
+        {
+            //Validate error display for incorrect inputs
+        }
+
+        [Test]
+        public void InteractHome()
+        {
+            //Interact with video, scroll and blog collection
+        }
+
+        [Test]
+        public void InteractBlog()
+        {
+            //Interact with scroll and blog collection
+        }
+
+        [Test]
+        public void InteractBlogDetailed()
+        {
+            //Interact with scroll, read more, and comments
+        }
+
+        [Test]
+        public void InteractEvent()
+        {
+            //Interact with calendar navigation
+        }
+
+        [Test]
+        public void InteractEventsDetailed()
+        {
+            //Interact with scroll, church location and RSVP
+        }
+
+        [Test]
+        public void InteractDonate()
+        {
+            //Interact with special message, donation amount and donate button
+        }
+
+        [Test]
+        public void InteractQR()
+        {
+            //possibly pass qr code into qr
+        }
+
+        [Test]
+        public void InteractAccount()
+        {
+            //Interact with newsletter, reset password and delete account
+        }
+
+        [Test]
+        public void InteractAccountEdit()
+        {
+            //Interact with editing fields and confirm changes taking place on accountpage and navflyout
+        }
+
+        [Test]
+        public void InvalidEntryLogin()
+        {
+            //Check error handling for incorrect inputs
+        }
+
+        [Test]
+        public void InvalidEntryRegister()
+        {
+            //Check error handling for incorrect inputs
+        }
+
+        [Test]
+        public void InvalidEntryAccountEdit()
+        {
+            //Check error handling for incorrect inputs for email and birthday
+        }
+
+        [Test]
+        public void InvalidEntryAccountResetPassword()
+        {
+            //Check error handling when incorrect password is given
+        }
+
+        [Test]
+        public void InvalidEntryAccountDelete()
+        {
+            //Check error handling when incorrect password is given
+        }
+
+        [Test]
+        public void InvalidEntryDonate()
+        {
+            //Check error handling when invalid donation amount is given
         }
     }
 }
